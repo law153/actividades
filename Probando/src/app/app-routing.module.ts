b2d3603b631a25fb51440af4559f5deac0fb713b
Abel <abelx3678@gmail.com>
2023-08-17 17:09:32 -0400

    creada copia para el proyecto

---
+++ Probando/src/app/app-routing.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'pagina1',
+    loadChildren: () => import('./pages/pagina1/pagina1.module').then( m => m.Pagina1PageModule)
+  },
+  {
+    path: 'pagina2',
+    loadChildren: () => import('./pages/pagina2/pagina2.module').then( m => m.Pagina2PageModule)
+  },
+  {
+    path: '**',
+    loadChildren: () => import('./pages/not-found/not-found.module').then( m => m.NotFoundPageModule)
+  },
+
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ Probando/src/app/home/home.page.ts
@@ -0,0 +1,89 @@
+import { Component, ViewChild } from '@angular/core';
+import { NavigationExtras, Router } from '@angular/router';
+import { AlertController, IonInput, ToastController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  //Variables
+  msj: string = 'Si ves esto, la alerta o mensaje no cambiaron correctamente';
+  nombreUsuario: string = 'Juan';
+  edad: number = 18;
+  user1: string = '';
+  clave: string = '';
+  persona: any = [
+    {
+      nombre: "Alexander",
+      edad: 20,
+      fecha: "08/03/2003"
+    }
+  ];
+
+  //Constructor
+
+  constructor(private router: Router, private alerta: AlertController, private tostada: ToastController ) {}
+  
+  //Metodos
+
+  @ViewChild('ionInputEl', { static: true }) ionInputEl!: IonInput;
+
+  onInput(ev: any) {
+    const value = ev.target!.value;
+
+    // Remover los valores no númericos
+    const filteredValue = value.replace(/[^0-9]+/g, '');
+
+    this.ionInputEl.value = this.clave = filteredValue;
+  }
+
+  irPagina1(){
+
+    if (this.user1.trim() === '' || this.clave.trim() === '') {
+
+      this.presentAlert('Por favor, completa todos los campos.');
+
+    } else if (this.user1.trim().length < 3 || this.clave.length < 4 ) {
+
+      this.presentAlert('Usuario o contraseña no validos!');
+
+    } else {
+      // Los campos son válidos, navegar a la siguiente página
+      let NavigationsExtra: NavigationExtras = {
+        state: {
+          nombreEnviar: this.user1
+        }
+      };
+      this.router.navigate(['/pagina1'], NavigationsExtra);
+    }
+
+    
+  }
+
+  //Alerta --- Investigar como usar este mismo codigo para mostrar distintas alertas (no copy paste cambiando cosas)
+  async presentAlert(mensaje: string) {
+    const alert = await this.alerta.create({
+      header: 'Alerta',
+      subHeader: 'Mensaje importante',
+      message: mensaje,
+      buttons: ['Vale'],
+    });
+
+    await alert.present();
+  }
+
+  async presentToast(position: 'top' | 'middle' | 'bottom', mensaje: string) {
+    const toast = await this.tostada.create({
+      message: mensaje,
+      duration: 2000,
+      position: position,
+    });
+
+    await toast.present();
+  }
+
+
+}
