22720afb617df8fea77bbefb1c97888a062df73d
Abel <abelx3678@gmail.com>
2023-08-23 12:12:34 -0400

    Cambios

--- proyecto3/src/app/home/home.page.ts
+++ proyecto3/src/app/home/home.page.ts
@@ -1,6 +1,7 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ElementRef, ViewChildren, ViewChild } from '@angular/core';
 import { NavigationExtras, Router } from '@angular/router';
-import { AlertController, IonInput, ToastController } from '@ionic/angular';
+import { AlertController, AnimationController, GestureController ,ToastController, IonCard, IonInput} from '@ionic/angular';
+import type { Animation, Gesture, GestureDetail } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -8,7 +9,7 @@ import { AlertController, IonInput, ToastController } from '@ionic/angular';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  
   //Variables
   msj: string = 'Si ves esto, la alerta o mensaje no cambiaron correctamente';
   nombreUsuario: string = 'Juan';
@@ -23,14 +24,84 @@ export class HomePage {
     }
   ];
 
+  listado: any = ["Juan", "Mario", "Alex"]
+
+
+  //Animación 
+  @ViewChild(IonCard, { read: ElementRef }) card!: ElementRef<HTMLIonCardElement>;
+
+  private animation!: Animation;
+  private gesture!: Gesture;
+  private started = false;
+  private initialStep = 0;
+
+  /**
+   * The track is 344px wide.
+   * The card is 100px wide.
+   * We want 16px of margin on each end of the track.
+   */
+  private readonly MAX_TRANSLATE = 344 - 100 - 32;
+
   //Constructor
 
-  constructor(private router: Router, private alerta: AlertController, private tostada: ToastController ) {}
+  constructor(private router: Router, private alerta: AlertController, private tostada: ToastController, private animationCtrl: AnimationController, private gestureCtrl: GestureController) {}
   
   //Metodos
 
-  @ViewChild('ionInputEl', { static: true }) ionInputEl!: IonInput;
+  private onMove(ev: GestureDetail) {
+    if (!this.started) {
+      this.animation.progressStart();
+      this.started = true;
+    }
 
+    this.animation.progressStep(this.getStep(ev));
+  }
+
+  private onEnd(ev: GestureDetail) {
+    if (!this.started) {
+      return;
+    }
+
+    this.gesture.enable(false);
+
+    const step = this.getStep(ev);
+    const shouldComplete = step > 0.5;
+
+    this.animation.progressEnd(shouldComplete ? 1 : 0, step).onFinish(() => {
+      this.gesture.enable(true);
+    });
+
+    this.initialStep = shouldComplete ? this.MAX_TRANSLATE : 0;
+    this.started = false;
+  }
+
+  private clamp(min: number, n: number, max: number) {
+    return Math.max(min, Math.min(n, max));
+  }
+
+  private getStep(ev: GestureDetail) {
+    const delta = this.initialStep + ev.deltaX;
+    return this.clamp(0, delta / this.MAX_TRANSLATE, 1);
+  }
+
+  ngAfterViewInit() {
+    this.animation = this.animationCtrl
+      .create()
+      .addElement(this.card.nativeElement)
+      .duration(1000)
+      .fromTo('transform', 'translateX(0)', `translateX(${this.MAX_TRANSLATE}px)`);
+
+    const gesture = (this.gesture = this.gestureCtrl.create({
+      el: this.card.nativeElement,
+      threshold: 0,
+      gestureName: 'card-drag',
+      onMove: (ev) => this.onMove(ev),
+      onEnd: (ev) => this.onEnd(ev),
+    }));
+
+    gesture.enable(true);
+  }
+  @ViewChild('ionInputEl', { static: true }) ionInputEl!: IonInput;
   onInput(ev: any) {
     const value = ev.target!.value;
 
@@ -62,6 +133,8 @@ export class HomePage {
 
     
   }
+
+  
 
   //Alerta --- Investigar como usar este mismo codigo para mostrar distintas alertas (no copy paste cambiando cosas)
   async presentAlert(mensaje: string) {
