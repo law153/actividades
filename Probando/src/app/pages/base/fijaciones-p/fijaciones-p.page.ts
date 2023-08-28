b3a0a6b2b433b24cb92cd8ab41bffe78ed03b8cd
Abel <abelx3678@gmail.com>
2023-08-28 17:52:34 -0400

    fix leve

--- Probando/src/app/pages/base/fijaciones-p/fijaciones-p.page.ts
+++ Probando/src/app/pages/base/fijaciones-p/fijaciones-p.page.ts
@@ -3,11 +3,11 @@ import { Router } from '@angular/router';
 import { MenuController } from '@ionic/angular';
 
 @Component({
-  selector: 'app-fijaciones-cli-p',
-  templateUrl: './fijaciones-cli-p.page.html',
-  styleUrls: ['./fijaciones-cli-p.page.scss'],
+  selector: 'app-fijaciones-p',
+  templateUrl: './fijaciones-p.page.html',
+  styleUrls: ['./fijaciones-p.page.scss'],
 })
-export class FijacionesCliPPage implements OnInit {
+export class FijacionesPPage implements OnInit {
 
   constructor(private menuCtrl: MenuController, private router: Router) { }
 
