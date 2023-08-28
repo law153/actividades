69b280de6e4331d76c79b28f63469fa830e6a36f
Abel <abelx3678@gmail.com>
2023-08-28 17:34:51 -0400

    Revert "adaptados los menu"
    This reverts commit 3b9305d0714eb95e29b3523c39fa106c051e4e45.

--- Probando/src/app/app.component.ts
+++ Probando/src/app/app.component.ts
@@ -7,17 +7,16 @@ import { NavigationExtras, Router } from '@angular/router';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-
-  usuario: number = 0 //0 = sin cuenta; 1 = cliente; 2 = admin
   constructor(private router: Router) {}
 
   //funciones de redirección
 
   //home
+  irHome(){
+    this.router.navigate([''])    
+  }
 
-  //Base ------------------------------------------------------------------
-
-  //Superior
+  //Miscelanio
   irIni(){
     this.router.navigate(['/ini-sesion']);
   }
@@ -56,96 +55,5 @@ export class AppComponent {
   irKits(){
     this.router.navigate(['/kits']);
   }
-
-  //Cliente ------------------------------------------------------------------------
-
-  //Superior
-  irPerfil(){
-    this.router.navigate(['/perfil']);
-  }
-  irCarrito(){
-    this.router.navigate(['/carrito']);
-  }
-  irContactanosCli(){
-    this.router.navigate(['/contactanos-cli']);
-  }
-  cerrarSesion(){
-    this.router.navigate(['']);
-  }
-
-  //categorias
-  irHerramientasCli(){
-    this.router.navigate(['/herramientas-cli']);
-  }
-
-  irFijacionesCli(){
-    this.router.navigate(['/fijaciones-cli']);
-  }
-
-  irElectricidadCli(){
-    this.router.navigate(['/electricidad-cli']);
-  }
-
-  irSeguridadCli(){
-    this.router.navigate(['/seguridad-cli']);
-  }
-
-  irRopaCli(){
-    this.router.navigate(['/ropa-cli']);
-  }
-
-  irGasfiteriaCli(){
-    this.router.navigate(['/gasfiteria-cli']);
-  }
-
-  irKitsCli(){
-    this.router.navigate(['/kits-cli']);
-  }
-
-  //Admin ------------------------------------------------------------------------
-
-  //Superior
-  irPerfilAdm(){
-    this.router.navigate(['/perfil-adm']);
-  }
-  irRoles(){
-    this.router.navigate(['/editar-rol']);
-  }
-  irConsultas(){
-    this.router.navigate(['/consultas']);
-  }
-  irPedidos(){
-    this.router.navigate(['/listado-pedido']);
-  }
-
-  //categorias
-  irHerramientasAdm(){
-    this.router.navigate(['/herramientas-adm']);
-  }
-
-  irFijacionesAdm(){
-    this.router.navigate(['/fijaciones-adm']);
-  }
-
-  irElectricidadAdm(){
-    this.router.navigate(['/electricidad-adm']);
-  }
-
-  irSeguridadAdm(){
-    this.router.navigate(['/seguridad-adm']);
-  }
-
-  irRopaAdm(){
-    this.router.navigate(['/ropa-adm']);
-  }
-
-  irGasfiteriaAdm(){
-    this.router.navigate(['/gasfiteria-adm']);
-  }
-
-  irKitsAdm(){
-    this.router.navigate(['/kits-adm']);
-  }
-
 
 }
