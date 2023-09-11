703572244c2331c95813ac1d76a343aadce983dd
law153 <abelx3678@gmail.com>
2023-09-11 12:47:26 -0300

    fixed

--- IvansMobile/src/app/services/dbservice.service.ts
+++ IvansMobile/src/app/services/dbservice.service.ts
@@ -1,16 +1,82 @@
-import { Injectable } from '@angular/core';
 
+import { Injectable } from '@angular/core';
+import { SQLiteObject, SQLite } from '@awesome-cordova-plugins/sqlite/ngx';
+import { Platform } from '@ionic/angular';
+import { BehaviorSubject } from 'rxjs';
+import { AlertController } from '@ionic/angular';
 @Injectable({
   providedIn: 'root'
 })
 export class DbserviceService {
+
+  //Conexion a base de datos
+  public database!: SQLiteObject;
+
+  //Variables de creación de tablas
   categoria: string = "CREATE TABLE IF NOT EXISTS categoria(id_categoria INTEGER PRIMARY KEY, nombre_categoria TEXT NOT NULL);";
   rol: string = "CREATE TABLE IF NOT EXISTS rol(id_rol INTEGER PRIMARY KEY, nombre_rol TEXT NOT NULL);";
   pregunta: string = "CREATE TABLE IF NOT EXISTS pregunta(id_pregunta INTEGER PRIMARY KEY, nombre_pregunta TEXT NOT NULL);";
   consulta: string = "CREATE TABLE IF NOT EXISTS consulta(id_consulta INTEGER PRIMARY KEY, nombre_consultante TEXT NOT NULL, asunto_consulta TEXT NOT NULL, mensaje_consulta TEXT NOT NULL);";
-  usuario: string = "CREATE TABLE IF NOT EXISTS usuario(id_usuario INTEGER PRIMARY KEY, rut INTEGER NOT NULL, dvrut CHAR(1) NOT NULL, nombre TEXT NOT NULL, apellido TEXT NOT NULL, telefono INTEGER NOT NULL, correo TEXT NOT NULL, clave TEXT NOT NULL, direccion TEXT NOT NULL, respuesta TEXT NOT NULL, id_rol INTEGER NOT NULL, id_pregunta INTEGER NOT NULL, FOREIGN KEY (id_rol) REFERENCES rol(id_rol), FOREIGN KEY (id_pregunta) REFERENCES pregunta(id_pregunta));";
-  producto: string = "CREATE TABLE IF NOT EXISTS producto(cod_prod INTEGER PRIMARY KEY, nombre_prod TEXT NOT NULL, descripcion TEXT NOT NULL, precio INTEGER NOT NULL, stock INTEGER NOT NULL, foto BLOB NOT NULL, unidad_medida TEXT NOT NULL, id_categoria INTEGER NOT NULL, FOREIGN KEY (id_categoria) REFERENCES categoria(id_categoria));";
-  venta: string = "CREATE TABLE IF NOT EXISTS venta(id_venta INTEGER PRIMARY KEY, fecha_venta DATE NOT NULL, estado TEXT NOT NULL, fecha_entrega DATE NOT NULL, total INTEGER NOT NULL, carrito CHAR(1), id_usuario INTEGER NOT NULL, FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario) );";
-  detalle: string= "CREATE TABLE IF NOT EXISTS detalle(id_detalle INTEGER PRIMARY KEY, cantidad INTEGER NOT NULL, subtotal INTEGER NOT NULL, id_venta INTEGER NOT NULL, cod_prod INTEGER NOT NULL, FOREIGN KEY (id_venta) REFERENCES venta(id_venta), FOREIGN KEY (cod_prod) REFERENCES producto(cod_prod) );";
-  constructor() { }
+  usuario: string = "CREATE TABLE IF NOT EXISTS usuario(id_usuario INTEGER PRIMARY KEY AUTOINCREMENT, rut INTEGER NOT NULL, dvrut CHAR(1) NOT NULL, nombre TEXT NOT NULL, apellido TEXT NOT NULL, telefono INTEGER NOT NULL, correo TEXT NOT NULL, clave TEXT NOT NULL, direccion TEXT NOT NULL, respuesta TEXT NOT NULL, rolU INTEGER NOT NULL, preguntaU INTEGER NOT NULL, FOREIGN KEY (rolU) REFERENCES rol(id_rol), FOREIGN KEY (preguntaU) REFERENCES pregunta(id_pregunta));";
+  producto: string = "CREATE TABLE IF NOT EXISTS producto(cod_prod INTEGER PRIMARY KEY AUTOINCREMENT, nombre_prod TEXT NOT NULL, descripcion TEXT NOT NULL, precio INTEGER NOT NULL, stock INTEGER NOT NULL, foto BLOB NOT NULL, unidad_medida TEXT NOT NULL, categoriaP INTEGER NOT NULL, FOREIGN KEY (categoriaP) REFERENCES categoria(id_categoria));";
+  venta: string = "CREATE TABLE IF NOT EXISTS venta(id_venta INTEGER PRIMARY KEY AUTOINCREMENT, fecha_venta DATE NOT NULL, estado TEXT NOT NULL, fecha_entrega DATE NOT NULL, total INTEGER NOT NULL, carrito CHAR(1), usuarioV INTEGER NOT NULL, FOREIGN KEY (usuarioV) REFERENCES usuario(id_usuario) );";
+  detalle: string= "CREATE TABLE IF NOT EXISTS detalle(id_detalle INTEGER PRIMARY KEY AUTOINCREMENT, cantidad INTEGER NOT NULL, subtotal INTEGER NOT NULL, ventaD INTEGER NOT NULL, productoD INTEGER NOT NULL, FOREIGN KEY (ventaD) REFERENCES venta(id_venta), FOREIGN KEY (productoD) REFERENCES producto(cod_prod) );";
+
+  //Variables para insert iniciales
+  rolCliente: string = "INSERT OR IGNORE INTO rol(id_rol, nombre_rol) VALUES (1, 'Cliente');";
+
+  //Observables 
+  listaRol = new BehaviorSubject([]);
+
+
+  //Observable estatus
+  private flag: BehaviorSubject<boolean> = new BehaviorSubject(false);
+
+  constructor(public base: SQLite, private plataforma: Platform, private alertController: AlertController) {
+    this.crearDB();
+   }
+
+  crearDB(){
+    //Plataforma lista
+    this.plataforma.ready().then(()=>{
+
+      this.base.create({name: 'ivans.db', location: 'default'
+    }).then((db: SQLiteObject)=>{
+      this.database = db;
+    }).catch(e=>{
+      this.presentAlert("Error al crear la base de datos");
+    })
+      
+
+    })
+  }
+
+  async crearTablas(){
+    try{
+      //Crear las tablas
+      await this.database.executeSql(this.rol,[]);
+
+      //Registros iniciales
+      await this.database.executeSql(this.rolCliente,[]);
+
+      this.flag.next(true);
+
+    }catch{
+      this.presentAlert("Error al crear la base de datos");
+    }
+  }
+
+
+  async presentAlert(mensaje: string) {
+    const alert = await this.alertController.create({
+      header: 'Alert',
+      subHeader: 'Important message',
+      message: mensaje,
+      buttons: ['OK'],
+    });
+
+    await alert.present();
+  }
+
+
 }
