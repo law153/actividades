5a879fa64ce6ce1da48a5a111376849af72f9a4f
alexignahdz <al.hernandezo@duocuc.cl>
2023-09-26 17:13:42 -0300

    arregle la camara creo
    soy un cabeza de fiambre

--- IvansMobile/src/app/pages/admin/agregar-prod/agregar-prod.page.ts
+++ IvansMobile/src/app/pages/admin/agregar-prod/agregar-prod.page.ts
@@ -31,7 +31,7 @@ export class AgregarProdPage implements OnInit {
   takePicture = async () => {
     const image = await Camera.getPhoto({
       quality: 90,
-      allowEditing: true,
+      allowEditing: false,
       resultType: CameraResultType.DataUrl,
       source: CameraSource.Prompt
     });
@@ -40,7 +40,7 @@ export class AgregarProdPage implements OnInit {
     // You can access the original file using image.path, which can be
     // passed to the Filesystem API to read the raw data of the image,
     // if desired (or pass resultType: CameraResultType.Base64 to getPhoto)
-    //var imagedataUrl = image.webPath;
+    //var imageUrl = image.webPath;
   
     // Can be set to the src of an image now
     this.foto = image.dataUrl!;
