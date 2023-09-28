64e4357a62d10aa5fcf3c53ffaff3bf849183814
Abel <abelx3678@gmail.com>
2023-09-28 18:37:13 -0300

    funciona el editar rol

--- IvansMobile/src/app/pages/admin/editar-rol/editar-rol.page.ts
+++ IvansMobile/src/app/pages/admin/editar-rol/editar-rol.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NavigationExtras, Router } from '@angular/router';
 import { MenuController } from '@ionic/angular';
 import { DbserviceService } from 'src/app/services/dbservice.service';
+import { PermisosService } from 'src/app/services/permisos.service';
 
 @Component({
   selector: 'app-editar-rol',
@@ -12,7 +13,7 @@ export class EditarRolPage implements OnInit {
 
   usuarios: any = [{idusuario: '',rut: '', dvrut: '', nombre: '', apellido: '', telefono: '', correo: '', clave: '', direccion: '', fotoUsuario: '', respuesta: '', rolu: '', preguntaU: ''}];
 
-  constructor(private menuCtrl: MenuController, private router: Router, private bd: DbserviceService) { }
+  constructor(private menuCtrl: MenuController, private router: Router, private bd: DbserviceService, private permisos: PermisosService) { }
 
   abrirSuperior(){
     this.menuCtrl.enable(true, 'superior');
@@ -41,10 +42,11 @@ export class EditarRolPage implements OnInit {
 
   cliente(id: number){
     this.bd.modificarRol(id, 1);
-
+    
   }
 
   admin(id: number){
     this.bd.modificarRol(id, 2);
+    
   }
 }
--- IvansMobile/src/app/pages/base/productos/productos.page.ts
+++ IvansMobile/src/app/pages/base/productos/productos.page.ts
@@ -11,13 +11,14 @@ import { PermisosService } from 'src/app/services/permisos.service';
 })
 export class ProductosPage implements OnInit {
   codprod: number = 0;
-  producto: any = {};
+  producto: any = [{codprod:'', nombreprod:'', descripcion: '', precio:'', stock: '', foto:'', unidadmedida: '', categoriap: ''}];
   permiso: number = 0;
   idusuario: number = 200;
-  venta: any = {};
+  venta: any = [{idventa:'', fechaventa: '', estado: '', fechaentrega:'', total:'', carrito: '', usuariov: ''}];
   fechaActual = new Date();
   diasSumar = 999;
   fechaEntrega = new Date(this.fechaActual);
+  detalle: any = [{iddetalle: '', cantidad: '', subtotal: '', ventad: '', productod: ''}];
   
   constructor(private menuCtrl: MenuController, private router: Router, private activeRouter: ActivatedRoute, private bd: DbserviceService,  private permisos: PermisosService) {
     this.activeRouter.queryParams.subscribe(param => {
@@ -56,13 +57,23 @@ export class ProductosPage implements OnInit {
           if (items.length > 0) {
             
             this.venta = items[0];
+            this.bd.buscarDetalleProd(this.producto.codprod);
+            this.bd.fetchDetalle().subscribe(details => {
+              if (details.length > 0) {
+                this.detalle = details[0];
+                this.bd.modificarDetalle(this.venta.idventa, this.detalle.subtotal+this.producto.precio, this.detalle.cantidad+1);
+                
+              } else{
+                this.bd.agregarDetalle(1, this.producto.precio, this.venta.idventa, this.producto.codprod);
+              }
 
+            })
             
           } else {
             // No se encontraron un carrito activo
             this.fechaEntrega.setDate(this.fechaActual.getDate() + this.diasSumar);
             this.venta = this.bd.agregarVenta(this.fechaActual, 'Activo', '11/11/2030', this.producto.precio, 'C', this.idusuario);
-            this.bd.agregarDetalle(1, this.producto.precio, this.venta, this.producto.codprod);
+            this.bd.agregarDetalle(1, this.producto.precio, this.venta.idventa, this.producto.codprod);
           }
         })
       }
