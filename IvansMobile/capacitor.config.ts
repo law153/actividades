d1de5c47f3504308edc274dbf4c900a1b151a3a1
alexignahdz <al.hernandezo@duocuc.cl>
2023-11-06 12:24:59 -0300

    avanzotes

--- IvansMobile/capacitor.config.ts
+++ IvansMobile/capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'proyecto3',
+  appName: 'Ivans',
   webDir: 'www',
   server: {
     androidScheme: 'https'
