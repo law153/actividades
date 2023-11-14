b7d7fcad802d359fd626f9ebcf96b96e8384f794
Abel <abelx3678@gmail.com>
2023-11-14 16:15:06 -0300

    aplicado qr y agentes

--- paginaIvans/core/views.py
+++ paginaIvans/core/views.py
@@ -2,4 +2,12 @@ from django.shortcuts import render, redirect
 
 # Create your views here.
 def mostrarIndex(request):
-    return render(request, 'core/index.html')
+    es_dispositivo_movil = verificar_dispositivo_movil(request)
+
+    context = {'es_dispositivo_movil': es_dispositivo_movil}
+
+    return render(request, 'core/index.html', context)
+
+def verificar_dispositivo_movil(request):
+    user_agent = request.META.get('HTTP_USER_AGENT', '').lower()
+    return 'mobile' in user_agent
